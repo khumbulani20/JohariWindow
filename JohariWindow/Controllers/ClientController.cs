7ac05d9002694746cc70c24b315b12529dbded2b
khumbulani20 <64993266+khumbulani20@users.noreply.github.com>
2021-10-12 11:52:29 -0600

    added admin folder added johari javascript added client controller added johari page

---
+++ JohariWindow/Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JohariWindow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ClientController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Client.List() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Client.Get(c => c.ClientID == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Client.Delete(objFromDb);
+            _unitOfWork.Commit();
+            return Json(new { success = true, message = "Delete successful " });
+        }
+
+    }
+}
+
---
+++ JohariWindow/wwwroot/js/johari.js
@@ -0,0 +1,66 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/client",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { data: "firstName", width: "40%" },
+            { data: "lastName", width: "40%" },
+            
+            {
+                data: "id", width: "30%",
+                "render": function (data) {
+                    return `<div class="text-center">
+
+                            <a href="/Admin/Johari?id=${data}"
+                            class ="btn btn-success text-white style="cursor:pointer; width=100px;"> <i class="far fa-edit"></i>Run Johari</a>
+
+                            <a onClick=Delete('/api/client/'+${data})
+                            class ="btn btn-danger text-white style="cursor:pointer; width=100px;"> <i class="far fa-trash-alt"></i>Delete</a>
+
+                    </div>`;
+                }
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+    //pass client id to johari window to use for running the johari
+}
+
+function Delete(url) {
+    //sweet alert 
+    swal({
+        title: "Are you sure want to delete ?",
+        text: "Once deleted, you will not be able to recover this  data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else
+                        toastr.error(data.message);
+                }
+
+            })
+        }
+    });
+}
